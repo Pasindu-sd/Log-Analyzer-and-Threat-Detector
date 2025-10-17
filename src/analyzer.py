a0ea33df437954c8ffb6ec5937fddf4ba042d7e6
Pasindu_SD <dilharawijesinghe86@gmail.com>
2025-10-17 21:21:25 +0530

    chnge files

--- src/analyzer.py
+++ src/analyzer.py
@@ -5,14 +5,14 @@ def read_log_file(file_path):
         with open(file_path, 'r') as f:
             return f.readlines()
     except FileNotFoundError:
-        print(f"Log file '{file_path}' not found !")
-        return[]
-    
+        print(f"Log file '{file_path}' not found!")
+        return []
+
 def failed_logins(log_lines):
     failed_ips = []
     for line in log_lines:
         if "Failed password" in line:
-            match = re.search(r'from (\d+\.\d+\.d+\.\d+)', line)
+            match = re.search(r'from (\d+\.\d+\.\d+\.\d+)', line)
             if match:
                 failed_ips.append(match.group(1))
     return failed_ips
